797bacb45121d3c9ead25e03a34f53982a603569
David García <wlogz@hotmail.com>
2023-03-12 14:55:46 -0500

    Agregando función eliminar img

--- app/peliculas/delete.php
+++ app/peliculas/delete.php
@@ -1,15 +1,26 @@
 <?php 
     require('../config/db.php');
 
+    session_start();
+
     $id = $conn -> real_escape_string($_POST['id']);
 
     $sql = "DELETE FROM pelicula
             WHERE id = $id";
 
     if ($conn -> query($sql)) {
-        
+
+        $dir = "posters";
+        $poster = $dir . '/' . $id . 'jpg';
+
+        if (file_exists($poster)) {
+            unlink($poster);
+        }
+        $_SESSION['color'] = "success";
+        $_SESSION['msg'] = "Registro eliminado de forma exitosa!";
+    } else {
+        $_SESSION['color'] = "danger";
+        $_SESSION['msg'] = "Error al eliminar el registro";
     }
 
     header('Location: index.php');
-
-?>
